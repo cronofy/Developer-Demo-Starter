2fd2ca59122553447b748fa14af3ac7016ede1c6
tomhazledine <tom@tomhazledine.com>
2019-08-08 14:49:51 +0100

    cleaning up app directory

--- app/server.js
+++ app/server.js
@@ -1,40 +1,104 @@
+// Import the dependencies
+const Cronofy = require("cronofy");
 const dotenv = require("dotenv");
-const enforce = require("express-sslify");
 const express = require("express");
 const bodyParser = require("body-parser");
-const session = require("cookie-session");
+const moment = require("moment");
 
+// Enable dotenv
 dotenv.config();
 
-const PORT = process.env.PORT || 7070;
-const URL = process.env.URL || `http://localhost:${PORT}`;
+// Setup
+const PORT = 7070;
 
 // Setup Express
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/"));
 app.set("view engine", "ejs");
 app.set("views", process.cwd() + "/app/templates");
-app.use(
-    session({
-        name: "developer_demo",
-        secret: "9U5AJIasFaaadz"
-    })
-);
-if (process.env.MODE !== "development") {
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
-}
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/"));
 
-const options = {
-    apiDomain: process.env.API_DOMAIN,
-    baseUrl: `${process.env.API_DOMAIN}/v1/element_tokens`,
-    url: URL
-};
+const cronofyClient = new Cronofy({
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    access_token: process.env.access_token
+});
 
-// Routes
-require("./routes/home")(app, options);
-require("./routes/availability")(app, options);
-require("./routes/submit")(app, options);
+// Route: home
+app.get("/", async (req, res) => {
+    const codeQuery = req.query.code;
+
+    if (codeQuery) {
+        const codeResponse = await cronofyClient.requestAccessToken({
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            grant_type: "authorization_code",
+            code: codeQuery,
+            redirect_uri: "http://localhost:7070"
+        });
+        session.access_token = codeResponse.access_token;
+    }
+
+    const token = await cronofyClient.requestElementToken({
+        version: "1",
+        permissions: ["managed_availability", "account_management"],
+        subs: [process.env.SUB],
+        origin: "http://localhost:7070"
+    });
+
+    return res.render("home", {
+        token: token.element_token.token,
+        client_id: process.env.CLIENT_ID
+    });
+});
+
+// Route: availability
+app.get("/availability", async (req, res) => {
+    const token = await cronofyClient.requestElementToken({
+        version: "1",
+        permissions: ["availability"],
+        subs: [process.env.SUB],
+        origin: "http://localhost:7070"
+    });
+
+    return res.render("availability", {
+        token: token.element_token.token,
+        sub: process.env.SUB
+    });
+});
+
+// Route: submit
+app.get("/submit", async (req, res) => {
+    const slot = JSON.parse(req.query.slot);
+
+    const userInfo = await cronofyClient.userInfo();
+
+    const calendarId =
+        userInfo["cronofy.data"].profiles[0].profile_calendars[0].calendar_id;
+
+    cronofyClient.createEvent({
+        calendar_id: calendarId,
+        event_id: "developer_demo_event",
+        summary: "Demo meeting",
+        description:
+            "Developer demo has created this event to show that events can be added to a calendar.",
+        start: slot.start,
+        end: slot.end,
+        location: {
+            description: "Board room"
+        }
+    });
+
+    const meetingDate = moment(slot.start).format("DD MMM YYYY");
+    const start = moment(slot.start).format("LT");
+    const end = moment(slot.end).format("LT");
+
+    return res.render("submit", {
+        meetingDate: meetingDate,
+        start: start,
+        end: end
+    });
+});
 
 app.listen(PORT);
-console.log(`serving on ${URL}`);
+console.log("serving on http://localhost:7070");
